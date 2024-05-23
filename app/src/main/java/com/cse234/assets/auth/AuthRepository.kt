d7e26b95cc926f1161399eae79cdeaa84df4ef20
ak1nttr <nsnsr6458@gmail.com>
2024-05-23 23:56:18 +0300

    some deletions

--- app/src/main/java/com/cse234/assets/auth/AuthRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.cse234.assets.auth
-
-import com.google.firebase.auth.AuthResult
-import kotlinx.coroutines.flow.Flow
-
-interface AuthRepository {
-   suspend fun loginUser(email: String, password: String): Flow<Resource<AuthResult>>
-    suspend fun registerUser(email: String, password: String): Flow<Resource<AuthResult>>
-}
