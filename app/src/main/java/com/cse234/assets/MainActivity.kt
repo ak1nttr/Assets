2fbe674e7d76f6c9fffe4eb119307612752e4dd1
ak1nttr <nsnr6458@gmail.com>
2024-06-07 16:23:00 +0300

    PersonalInfoScreen ve HeightWeightScreen yapıldı ana ekrana navigation composablelar eklendi (sayfaların db işlemleri henüz yok)

--- app/src/main/java/com/cse234/assets/MainActivity.kt
+++ app/src/main/java/com/cse234/assets/MainActivity.kt
@@ -11,10 +11,12 @@ import androidx.navigation.compose.rememberNavController
 import com.cse234.assets.screens.ActivitiesScreen
 import com.cse234.assets.screens.ActivityHistoryScreen
 import com.cse234.assets.screens.ActivityViewModel
+import com.cse234.assets.screens.HeightWeightScreen
 import com.cse234.assets.screens.HomeScreen
 import com.cse234.assets.screens.LoginProcedure
 import com.cse234.assets.screens.LoginScreen
 import com.cse234.assets.screens.NotificationScreen
+import com.cse234.assets.screens.PersonalInfoScreen
 import com.cse234.assets.screens.RegisterProcedure
 import com.cse234.assets.screens.TimerScreenContent
 import com.cse234.assets.screens.TimerViewModel
@@ -61,6 +63,12 @@ class MainActivity : ComponentActivity() {
                     }
                     composable("NotificationScreen"){
                         NotificationScreen(navController)
+                    }
+                    composable("PersonalInfoScreen"){
+                        PersonalInfoScreen(navController = navController)
+                    }
+                    composable("HeightWeightScreen"){
+                        HeightWeightScreen(navController = navController)
                     }
                 }
             }
