5145b57e159b7837bb6cc8c2061401caf4676692
ak1nttr <nsnr6458@gmail.com>
2024-06-07 15:04:16 +0300

    UserInfo has been added

---
+++ app/src/main/java/com/cse234/assets/data/UserInfo.kt
@@ -0,0 +1,14 @@
+package com.cse234.assets.data
+
+
+import java.util.Date
+
+data class UserInfo(
+    val email : String,
+    val gender :  String,
+    val height : Double,
+    val weight : Double,
+    val birthDate : Date
+){
+    constructor() : this("","",0.0,0.0, Date())
+}
